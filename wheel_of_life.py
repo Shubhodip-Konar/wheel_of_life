b1ddb3d1ee631147b8de8a20cb9ef71fe8820276
Shubhodip Konar <41413092+Shubhodip-Konar@users.noreply.github.com>
2026-04-03 17:20:13 +0530

    Update wheel_of_life.py

--- wheel_of_life.py
+++ wheel_of_life.py
@@ -75,6 +75,7 @@ def plot_polar_wheel(scores_dict):
     #axis settings
     ax.set_ylim(0, 10)
     ax.set_yticks(range(1,11))
+    ax.set_yticklabels([])
     ax.set_xticks([])
     ax.set_title("Your Life Balance", pad=20)
     ax.legend(bars, labels, loc="center left", bbox_to_anchor=(1.1, 0.5), title="Life Areas")
